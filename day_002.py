267f41af220e52d8fcc0d6540bad3b9e8c17feb7
sks1234ty-ship-it <sks1234ty@gmail.com>
2026-07-02 22:46:15 +0530

    Day 002: variables, input, f-string

---
+++ day_002.py
@@ -0,0 +1,10 @@
+# Day 002 - Variables, Input, and F-Strings
+# Goal: Ask name and age, calculate future age, print message
+# Date: 2026
+name = input("What is your name? ")
+age = int(input("what is your age?"))
+future_age = age + 7
+
+print(name)
+print(age)
+print(f"new age: {future_age}")
